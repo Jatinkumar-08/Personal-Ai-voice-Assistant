7b938f6baaf9aa99899041e7224140c8b443f395
JATIN KUMAR <jatinone92@gmail.com>
2025-04-27 12:02:01 +0530

    Add files via upload

---
+++ MySongLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "afsos": "https://youtu.be/2FhgKp_lfJQ?si=k2RBhXAe-LQhdssU",
+    "raanjhan": "https://youtu.be/lBvbNxiVmZA?si=OrQtEt9Y2d_AV4Cd",
+    "forever": "https://youtu.be/8wpDeaFi4FI?si=zOz5mtfGl_hNThPY",
+    "senorita": "https://youtu.be/Pkh8UtuejGw?si=pwEQqUOOgQ9JwITB"
+}
